3cf541b2be264490688729441c614a28265a8e86
rawsashimi1604 <looweiren@gmail.com>
2023-08-09 21:58:24 +0800

    added gitignore and psql connection

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"strconv"
 
+	"github.com/rawsashimi1604/go-stock-api/middleware"
 	"github.com/rawsashimi1604/go-stock-api/router"
 )
 
@@ -12,6 +13,9 @@ func main() {
 	fmt.Println("Starting the server.")
 	port := 8080
 	router := router.NewRouter()
+
+	db := middleware.CreateConnection()
+	db.Ping()
 
 	err := http.ListenAndServe(":"+strconv.Itoa(port), router)
 	if err != nil {
--- middleware/handlers.go
+++ middleware/handlers.go
@@ -1,23 +1,48 @@
 package middleware
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
-type Response struct {
-	Message string `json:"message"`
-	Code    int    `json:"code"`
+type response struct {
+	Message string `json:"message,omitempty"`
+	Code    int    `json:"code,omitempty"`
+}
+
+func CreateConnection() *sql.DB {
+	if err := godotenv.Load(".env"); err != nil {
+		log.Fatal("Something went wrong when loading the .env file.")
+	}
+
+	db, err := sql.Open("postgres", os.Getenv("POSTGRES_URL"))
+	if err != nil {
+		log.Println(err)
+		log.Fatal("Something went wrong when connecting to the db.")
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Successfully connected to postgres.")
+	return db
 }
 
 func HandleIndex(writer http.ResponseWriter, request *http.Request) {
 	msg := "Hello world from index! Path: /"
 	fmt.Println(msg)
 
-	response := Response{
+	response := response{
 		Message: msg,
-		Code:    200,
 	}
 
 	writer.Header().Set("Content-Type", "application.json")
